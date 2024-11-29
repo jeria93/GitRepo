9be525700dbac9935d2126f8e757c9ec2562ed8d
Nicholas <39989194+jeria93@users.noreply.github.com>
2024-11-29 19:49:21 +0100

    added design

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.git1"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.git1"
--- app/src/main/java/com/example/git1/MainActivity.kt
+++ app/src/main/java/com/example/git1/MainActivity.kt
@@ -10,6 +10,7 @@ import com.example.git1.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
